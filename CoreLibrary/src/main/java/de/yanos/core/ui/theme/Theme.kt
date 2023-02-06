39e1d13bb6ba07b2cf7292b80dfc74ca61f22da6
sse <sse@mycraftnote.de>
2023-02-06 01:33:46 +0100

    fix R import

--- CoreLibrary/src/main/java/de/yanos/core/ui/theme/Theme.kt
+++ CoreLibrary/src/main/java/de/yanos/core/ui/theme/Theme.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import de.yanos.core.R
+import de.yanos.corelibrary.R
 
 val Montserrat = FontFamily(
     Font(R.font.montserrat_medium, FontWeight.Medium),
