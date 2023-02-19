d30bafdd894cf49e6913adbf899ff137e2ba81c7
yanosDev <sonaysenguen@gmail.com>
2023-02-19 22:34:14 +0100

    update version

--- CoreLibrary/build.gradle.kts
+++ CoreLibrary/build.gradle.kts
@@ -80,7 +80,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "de.yanos"
             artifactId = "corelibrary"
-            version = "0.1.14"
+            version = "0.1.15"
 
             afterEvaluate {
                 from(components["release"])
