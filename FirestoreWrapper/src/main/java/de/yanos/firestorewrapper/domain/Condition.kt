1e0e4021a1472dc5d24d7b88c98b2bcab087ed3a
yanosDev <sonaysenguen@gmail.com>
2023-02-19 14:37:24 +0100

    finalize path logic

--- FirestoreWrapper/src/main/java/de/yanos/firestorewrapper/domain/Condition.kt
+++ FirestoreWrapper/src/main/java/de/yanos/firestorewrapper/domain/Condition.kt
@@ -1,12 +1,12 @@
 package de.yanos.firestorewrapper.domain
 
-sealed class Condition(open val priority: Int) {
-    class Limit(val count: Long) : Condition(0)
-    class OrderByAscending(val field: String) : Condition(1)
-    class OrderByDescending(val field: String) : Condition(2)
-    class WhereArrayContains(val field: String, val value: Any) : Condition(3)
-    class WhereLessThan(val field: String, val value: Any) : Condition(4)
-    class WhereGreaterThan(val field: String, val value: Any) : Condition(5)
-    class WhereIn(val field: String, val values: List<Any>) : Condition(6)
-    class WhereEquals(val field: String,  val value: Any) : Condition(7)
+sealed class Condition(open val priority: Int, val uniqueId: String) {
+    class Limit(val count: Long) : Condition(0, count.toString())
+    class OrderByAscending(val field: String) : Condition(1, field)
+    class OrderByDescending(val field: String) : Condition(2, field)
+    class WhereArrayContains(val field: String, val value: Any) : Condition(3, value.toString())
+    class WhereLessThan(val field: String, val value: Any) : Condition(4, value.toString())
+    class WhereGreaterThan(val field: String, val value: Any) : Condition(5, value.toString())
+    class WhereIn(val field: String, val values: List<Any>) : Condition(6, values.joinToString { "::" })
+    class WhereEquals(val field: String, val value: Any) : Condition(7, value.toString())
 }
--- FirestoreWrapper/src/main/java/de/yanos/firestorewrapper/util/FirestorePath.kt
+++ FirestoreWrapper/src/main/java/de/yanos/firestorewrapper/util/FirestorePath.kt
@@ -2,45 +2,77 @@ package de.yanos.firestorewrapper.util
 
 import de.yanos.firestorewrapper.domain.Condition
 
-class FirestorePath(val path: List<String>, conditions: MutableList<Condition>)
+interface FirestorePath<T> {
+    val id: String
+}
 
-sealed class FirestorePathBuilder<T>(
-    open val paths: MutableList<String> = mutableListOf(),
-    open val conditions: MutableList<Condition> = mutableListOf()
-) {
-    fun build(): FirestorePath {
-        return FirestorePath(paths, conditions)
+internal data class FirestorePathImpl<T>(val path: List<String>, val conditions: MutableList<Condition>, val clazz: Class<T>) : FirestorePath<T> {
+    override val id
+        get() =
+            "${path.joinToString(separator = "/")} ${
+                conditions.sortedBy { it.priority }.joinToString { condition -> condition.priority.toString() + condition.uniqueId }
+            }"
+}
+
+sealed interface FirestorePathBuilder<T> {
+    val paths: MutableList<String>
+    val conditions: MutableList<Condition>
+    val clazz: Class<T>
+    fun build(): FirestorePath<T> {
+        return FirestorePathImpl(paths, conditions, clazz)
     }
 
-    class DocumentPathBuilder<T>(
-        override val paths: MutableList<String>,
-        override val conditions: MutableList<Condition>
-    ) : FirestorePathBuilder<T>() {
-        fun collection(path: String): CollectionPathBuilder<T> {
-            return CollectionPathBuilder(paths.apply { add(path) }, conditions)
+    companion object {
+        fun <T> Builder(clazz: Class<T>): CollectionPathBuilder<T> {
+            return CollectionPathBuilderImpl<T>(mutableListOf(), mutableListOf(), clazz)
         }
     }
+}
 
-    class CollectionPathBuilder<T>(
-        override val paths: MutableList<String>,
-        override val conditions: MutableList<Condition>
-    ) : FirestorePathBuilder<T>() {
+interface DocumentPathBuilder<T> : FirestorePathBuilder<T> {
+    fun collection(path: String): CollectionPathBuilder<T>
+}
 
-        fun document(path: String): DocumentPathBuilder<T> {
-            return DocumentPathBuilder(paths.apply { add(path) }, conditions)
-        }
+interface CollectionPathBuilder<T> : FirestorePathBuilder<T> {
+    fun document(path: String): DocumentPathBuilder<T>
+    fun condition(condition: Condition): QueryPathBuilder<T>
+}
 
-        fun condition(condition: Condition): QueryPathBuilder<T> {
-            return QueryPathBuilder(paths, conditions.apply { add(condition) })
-        }
+interface QueryPathBuilder<T> : FirestorePathBuilder<T> {
+    fun condition(condition: Condition): QueryPathBuilder<T>
+}
+
+
+class DocumentPathBuilderImpl<T>(
+    override val paths: MutableList<String>,
+    override val conditions: MutableList<Condition>,
+    override val clazz: Class<T>
+) : DocumentPathBuilder<T> {
+    override fun collection(path: String): CollectionPathBuilder<T> {
+        return CollectionPathBuilderImpl(paths.apply { add(path) }, conditions, clazz)
+    }
+}
+
+class CollectionPathBuilderImpl<T>(
+    override val paths: MutableList<String>,
+    override val conditions: MutableList<Condition>,
+    override val clazz: Class<T>
+) : CollectionPathBuilder<T> {
+    override fun document(path: String): DocumentPathBuilder<T> {
+        return DocumentPathBuilderImpl(paths.apply { add(path) }, conditions, clazz)
     }
 
-    class QueryPathBuilder<T>(
-        override val paths: MutableList<String>,
-        override val conditions: MutableList<Condition>
-    ) : FirestorePathBuilder<T>() {
-        fun condition(condition: Condition): QueryPathBuilder<T> {
-            return QueryPathBuilder(paths, conditions.apply { add(condition) })
-        }
+    override fun condition(condition: Condition): QueryPathBuilder<T> {
+        return QueryPathBuilderImpl(paths, conditions.apply { add(condition) }, clazz)
+    }
+}
+
+class QueryPathBuilderImpl<T>(
+    override val paths: MutableList<String>,
+    override val conditions: MutableList<Condition>,
+    override val clazz: Class<T>
+) : QueryPathBuilder<T> {
+    override fun condition(condition: Condition): QueryPathBuilder<T> {
+        return QueryPathBuilderImpl(paths, conditions.apply { add(condition) }, clazz)
     }
 }
