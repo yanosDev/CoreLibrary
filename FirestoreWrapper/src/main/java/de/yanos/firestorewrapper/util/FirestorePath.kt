7aa523c586dd02c6679ea1428887edda16e8081e
yanosDev <sonaysenguen@gmail.com>
2023-02-19 13:46:50 +0100

    implement path logic

--- FirestoreWrapper/src/main/java/de/yanos/firestorewrapper/util/FirestorePath.kt
+++ FirestoreWrapper/src/main/java/de/yanos/firestorewrapper/util/FirestorePath.kt
@@ -1,8 +1,46 @@
 package de.yanos.firestorewrapper.util
 
-class FirestorePath {
-}
+import de.yanos.firestorewrapper.domain.Condition
 
-class FirestorePathBuilder<T> {
+class FirestorePath(val path: List<String>, conditions: MutableList<Condition>)
 
+sealed class FirestorePathBuilder<T>(
+    open val paths: MutableList<String> = mutableListOf(),
+    open val conditions: MutableList<Condition> = mutableListOf()
+) {
+    fun build(): FirestorePath {
+        return FirestorePath(paths, conditions)
+    }
+
+    class DocumentPathBuilder<T>(
+        override val paths: MutableList<String>,
+        override val conditions: MutableList<Condition>
+    ) : FirestorePathBuilder<T>() {
+        fun collection(path: String): CollectionPathBuilder<T> {
+            return CollectionPathBuilder(paths.apply { add(path) }, conditions)
+        }
+    }
+
+    class CollectionPathBuilder<T>(
+        override val paths: MutableList<String>,
+        override val conditions: MutableList<Condition>
+    ) : FirestorePathBuilder<T>() {
+
+        fun document(path: String): DocumentPathBuilder<T> {
+            return DocumentPathBuilder(paths.apply { add(path) }, conditions)
+        }
+
+        fun condition(condition: Condition): QueryPathBuilder<T> {
+            return QueryPathBuilder(paths, conditions.apply { add(condition) })
+        }
+    }
+
+    class QueryPathBuilder<T>(
+        override val paths: MutableList<String>,
+        override val conditions: MutableList<Condition>
+    ) : FirestorePathBuilder<T>() {
+        fun condition(condition: Condition): QueryPathBuilder<T> {
+            return QueryPathBuilder(paths, conditions.apply { add(condition) })
+        }
+    }
 }
