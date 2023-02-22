a6193cfa2b8aa5c205dbed681f1a1fa5224eec21
yanosDev <sonaysenguen@gmail.com>
2023-02-22 22:04:29 +0100

    improve auth

--- FirestoreWrapper/src/main/java/de/yanos/firestorewrapper/ui/AuthenticationActivity.kt
+++ FirestoreWrapper/src/main/java/de/yanos/firestorewrapper/ui/AuthenticationActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package de.yanos.firestorewrapper.ui
 
 import android.app.Activity
@@ -7,20 +9,37 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Login
+import androidx.compose.material.icons.rounded.Logout
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.android.gms.auth.api.identity.SignInClient
 import de.yanos.corelibrary.ui.theme.SonayPreviews
 import de.yanos.crashlog.util.Clog
+import de.yanos.firestorewrapper.R
 import de.yanos.firestorewrapper.domain.AuthRepositoryBuilder
 import de.yanos.firestorewrapper.domain.AuthResult
 import kotlinx.coroutines.launch
@@ -71,42 +90,92 @@ private fun AuthScreen(modifier: Modifier = Modifier, isUserLoggedIn: Boolean = 
 
 @Composable
 private fun SignInScreen(modifier: Modifier = Modifier, signInClick: () -> Unit = {}) {
-    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(
-            onClick = signInClick,
-            modifier = Modifier.padding(16.dp),
-            shape = RoundedCornerShape(6.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Black,
-                contentColor = Color.White
-            )
-        ) {
-            Image(
-                painter = painterResource(id = com.google.android.gms.base.R.drawable.common_full_open_on_phone),
-                contentDescription = ""
-            )
-            Text(text = "Sign in with Google", modifier = Modifier.padding(6.dp))
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        val username = remember { mutableStateOf(TextFieldValue()) }
+        val password = remember { mutableStateOf(TextFieldValue()) }
+
+        Text(
+            text = stringResource(id = R.string.login),
+            style = MaterialTheme.typography.headlineLarge.copy(
+                shadow = Shadow(
+                    color = MaterialTheme.colorScheme.onBackground,
+                    offset = Offset(5f, 5f),
+                    blurRadius = 15f
+                ),
+                fontStyle = FontStyle.Italic
+            ),
+            color = MaterialTheme.colorScheme.onBackground
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Username") },
+            value = username.value,
+            onValueChange = { username.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Password") },
+            value = password.value,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            onValueChange = { password.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = { },
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(text = "Login")
+            }
         }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        ClickableText(
+            text = AnnotatedString("Forgot password?"),
+            onClick = { },
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+    Button(
+        onClick = signInClick,
+        modifier = Modifier.padding(16.dp),
+        shape = RoundedCornerShape(6.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Image(
+            imageVector = Icons.Rounded.Login,
+            contentDescription = "",
+            colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.primary)
+        )
+        Text(text = stringResource(id = R.string.btn_login_google), modifier = Modifier.padding(6.dp))
     }
 }
 
 @Composable
 private fun SignOffScreen(modifier: Modifier = Modifier, signOffClick: () -> Unit = {}) {
-    Column(modifier = modifier, verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally) {
         Button(
             onClick = signOffClick,
             modifier = Modifier.padding(16.dp),
             shape = RoundedCornerShape(6.dp),
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Black,
-                contentColor = Color.White
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
             )
         ) {
-            Image(
-                painter = painterResource(id = com.google.android.gms.base.R.drawable.common_full_open_on_phone),
-                contentDescription = ""
+            Icon(
+                imageVector = Icons.Rounded.Logout,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.onPrimaryContainer
             )
-            Text(text = "Sign Off with Google", modifier = Modifier.padding(6.dp))
+            Text(text = stringResource(id = R.string.btn_logout), modifier = Modifier.padding(6.dp))
         }
     }
 }
